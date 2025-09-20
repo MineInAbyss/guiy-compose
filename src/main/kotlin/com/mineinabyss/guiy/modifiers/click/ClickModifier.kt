324f66727b2880983f7753f164ef6754d39f7bc4
Danielle Voznyy <dan.voznyy@gmail.com>
2025-09-19 20:48:39 -0400

    fix: Ignore DOUBLE_CLICK events in clickable modifier causing double inputs, creating a separate onClickEvent modifier for old behavior.

--- src/main/kotlin/com/mineinabyss/guiy/modifiers/click/ClickModifier.kt
+++ src/main/kotlin/com/mineinabyss/guiy/modifiers/click/ClickModifier.kt
@@ -1,6 +1,7 @@
 package com.mineinabyss.guiy.modifiers.click
 
 import com.mineinabyss.guiy.modifiers.Modifier
+import org.bukkit.event.inventory.ClickType
 
 open class ClickModifier(
     val merged: Boolean = false,
@@ -16,22 +17,31 @@ open class ClickModifier(
                 onClick()
             other.onClick(this)
         },
-        /*allowClick = {
-                   if (!other.merged)
-                       allowClick()
-                   other.allowClick(this)
-               }*/
     )
 }
 
+/**
+ * A click listener. Will ignore double clicks as Minecraft sends two individual click events
+ * AND a double click. To handle these events manually, use [onClickEvent].
+ */
 fun Modifier.clickable(
     consumeClick: Boolean = false,
-//    allowClick: ClickScope.() -> Boolean = { true },
-    onClick: ClickScope.() -> Unit
-) =
-    then(
-        ClickModifier(
-            consumeClick = consumeClick,
-            onClick = onClick, /*allowClick = allowClick*/
-        )
+    onClick: ClickScope.() -> Unit,
+) = onClickEvent(consumeClick) {
+    if (this.clickType != ClickType.DOUBLE_CLICK) onClick()
+}
+
+/**
+ * A click listener, passing through all inventory click events in this composable's bounds.
+ *
+ * NOTE: This passes a DOUBLE_CLICK event as well as two individual click events. Use [clickable] if this is not desired.
+ */
+fun Modifier.onClickEvent(
+    consumeClick: Boolean = false,
+    onClick: ClickScope.() -> Unit,
+) = then(
+    ClickModifier(
+        consumeClick = consumeClick,
+        onClick = onClick,
     )
+)
